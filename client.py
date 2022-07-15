f65d802a241404500cfa70cd790cbb1224c8673b
Oliver Cocker <oliver.cocker@outlook.com>
2022-07-15 09:37:28 +0100

    Create Patch File

--- client.py
+++ client.py
@@ -18,7 +18,7 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import urllib2
+import urllib
 import time
 import json
 import random
@@ -29,31 +29,38 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-def getDataPoint(quote):
+
+def getDataPoint(quote1):
 	""" Produce all of the needed values to generate a datapoint """
 	""" ------------- Update this function ------------- """
-	stock = quote['stock']
-	bid_price = float(quote['top_bid']['price'])
-	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
-	return stock, bid_price, ask_price, price
+	stock1 = quote1['stock']
+	bid_price1 = float(quote['top_bid']['price'])
+	ask_price1 = float(quote['top_ask']['price'])
+	price1 = (bid_price + ask_price)/2
+	return stock1, bid_price1, ask_price1, price1
+
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
+	if price_b == 0:
+		return
+	return price_a/price_b
+
 
 # Main
 if __name__ == "__main__":
 
 	# Query the price once every N seconds.
-	for _ in xrange(N):
-		quotes = json.loads(urllib2.urlopen(QUERY.format(random.random())).read())
+	for _ in range(N):
+		quotes = json.loads(urllib.Request.urlopen(QUERY.format(random.random())).read())
 
 		""" ----------- Update to get the ratio --------------- """
+		prices = {}
 		for quote in quotes:
 			stock, bid_price, ask_price, price = getDataPoint(quote)
-			print "Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price)
+			prices[stock] = price
+			print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-		print "Ratio %s" % getRatio(price, price)
+		print("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
